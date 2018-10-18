2af08475a7eda71e7290b1073fb8653c45bb5b78
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-18 01:16:16 -0400

    Header update

--- react-portfolio/src/component/Header.js
+++ react-portfolio/src/component/Header.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import Background from '../background.jpg'
+import './Header.css'
+
+const myStyles = {
+    backgroundImage: `url(${Background})`,
+    height: '100vh',
+    backgroundSize: 'cover'
+} 
+
 
 class Header extends Component {
+    
     render(){
         return (
-            
+            <header style={myStyles}>
+                <h1>{this.props.title}</h1>
+                <p>A Simple Portfolio created with React</p>
+                <a href="#header" >{this.props.button}</a>
+            </header>
         )
     }
 }
