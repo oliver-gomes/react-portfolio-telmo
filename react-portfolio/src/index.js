79d808b2435d0ef2391a8ab7dfb9012f4e6dd9b1
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-17 23:55:53 -0400

    App update

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './container/App';
+import App from './container/App';
 import * as serviceWorker from './serviceWorker';
-import Navigation from './component/Navigation'
 
-ReactDOM.render(<Navigation />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(<App />, document.getElementById('root'));
+
 serviceWorker.unregister();
