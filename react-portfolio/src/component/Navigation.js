326a6c04b9a862f99ac01d37f2fd11a5f5739c2a
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-17 20:54:35 -0400

    Nav menu setup

--- react-portfolio/src/component/Navigation.js
+++ react-portfolio/src/component/Navigation.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-
+import './Navigation.css'
 
 class Navigation extends Component {
   render() {
     return (
-      
+      <div>
+        <nav>
+            <h2 className="logo">React Project</h2>
+
+            <ul>
+              <li> <a href="#">Home</a></li>
+              <li> <a href="#">About</a> </li>
+              <li> <a href="#">Services</a></li>
+              <li> <a href="#">Portfolio</a></li>
+              <li> <a href="#">Contact</a></li>
+            </ul>
+        </nav>
+      </div>
     );
   }
 }
